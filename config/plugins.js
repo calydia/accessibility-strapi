e3d8dcd8e0a4a7ae71f1a13e9d4edd63282907cb
Sanna Mäkinen <sanna.makinen@gmail.com>
2023-03-24 19:15:58 +0200

    Sitemap plugin config

--- config/plugins.js
+++ config/plugins.js
@@ -17,4 +17,11 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  sitemap: {
+    enabled: true,
+    config: {
+      autoGenerate: true,
+      allowedFields: ['id', 'uid', 'pageUrl']
+    }
+  }
 });
