922b5ab2ff42b99b5137b6f88409a80ed56eb145
Kari Honkasalo <kari.honkasalo@gmail.com>
2023-09-17 17:20:37 +0300

    feat: Add strapi-plugin-netlify-deployments

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   ckeditor: {
-    enabled: true
+    enabled: true,
   },
   navigation: {
-    enabled: true
+    enabled: true,
   },
   graphql: {
     config: {
-      endpoint: '/graphql',
+      endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: false,
       depthLimit: 7,
@@ -21,23 +21,40 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       autoGenerate: true,
-      allowedFields: ['id', 'uid', 'pageUrl']
-    }
+      allowedFields: ["id", "uid", "pageUrl"],
+    },
   },
   meilisearch: {
     config: {
-      host: env('MEILI_HTTP_PROTOCOL', 'http') + "://" + env('MEILI_HTTP_ADDR', 'localhost:7700'),
-      apiKey: env('MEILI_MASTER_KEY', 'wrong_value'),
-      "page": {
+      host:
+        env("MEILI_HTTP_PROTOCOL", "http") +
+        "://" +
+        env("MEILI_HTTP_ADDR", "localhost:7700"),
+      apiKey: env("MEILI_MASTER_KEY", "wrong_value"),
+      page: {
         entriesQuery: {
-          locale: 'all'
-        }
+          locale: "all",
+        },
       },
       "demo-page": {
         entriesQuery: {
-          locale: 'all'
-        }
-      }
-    }
-  }
+          locale: "all",
+        },
+      },
+    },
+  },
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: env("NETLIFY_PERSONAL_ACCESS_TOKEN", "wrong_value"),
+      sites: [
+        {
+          name: env("NETLIFY_SITE_NAME", "Netlify Front End"),
+          id: env("NETLIFY_SITE_ID", "wrong_value"),
+          buildHook: env("NETLIFY_BUILD_HOOK_URL", "wrong_value"),
+          branch: env("NETLIFY_GIT_BRANCH", "master"),
+        },
+      ],
+    },
+  },
 });
